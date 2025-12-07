17ac7da1a4d8d88025849ca46ebc8b3b82c46b60
Artyk Basarov <info@artyk.co.uk>
2025-12-07 20:53:59 +0000

    chore: update Desk softphone button icon

--- telephony/public/js/sip_softphone.js
+++ telephony/public/js/sip_softphone.js
@@ -53,6 +53,9 @@ frappe.provide("telephony.sip");
   color: #fff;
   font-size: 24px;
   cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   box-shadow: 0 10px 30px rgba(15, 23, 42, 0.4);
   transition: transform 0.2s ease, box-shadow 0.2s ease;
 }
@@ -581,8 +584,10 @@ frappe.provide("telephony.sip");
       this.root = document.createElement("div");
       this.root.id = SOFTPHONE_ID;
       this.root.innerHTML = `
-        <button type="button" class="tp-softphone-toggle" aria-expanded="false">
-          <span aria-hidden="true">☎</span>
+        <button type="button" class="tp-softphone-toggle" aria-expanded="false" aria-label="Desk softphone">
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 16 16" aria-hidden="true">
+            <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z"/>
+          </svg>
         </button>
         <div class="tp-softphone-panel" aria-hidden="true">
           <div class="tp-softphone-content">
